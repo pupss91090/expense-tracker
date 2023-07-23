51210ac2d9fb175f0a6a0599e2e9a43a70389580
pupss91090 <hsuchinzai@gmail.com>
2023-07-23 13:18:49 +0800

    add login&register hbs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const record = require('./modules/record')
+const users = require('./modules/users')
 
+router.use('/users', users)
 router.use('/record', record)
 router.use('/', home)
 
